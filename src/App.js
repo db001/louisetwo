dd2d01fc787e658759892a7169d3c1f6fb212bbc
db001 <dnbennett@hotmail.co.uk>
2019-04-09 22:51:59 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
+import Header from './components/Header/Header';
+import Homepage from './components/Homepage/Homepage';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Projects from './components/Projects/Projects';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <BrowserRouter>
+                <div>
+                    <Header />
+                </div>
+                <Switch>
+                    <Route exact path="/" component={Homepage} />
+                    <Route path="/about" component={About} />
+                    <Route path="/contact" component={Contact} />
+                    <Route path="/projects" component={Projects} />
+                </Switch>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
